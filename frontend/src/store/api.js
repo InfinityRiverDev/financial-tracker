4cd1b6f700c7325cd0f034ee2c2f65e178f98b0a
Rishat <rishatfattahov22@gmail.com>
2025-12-16 03:48:35 +0300

    create backend url and push in frontend

--- frontend/src/store/api.js
+++ frontend/src/store/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const transactionsApi = createApi({
   reducerPath: "transactionsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://financial-tracker-t5qu.onrender.com" }),
   tagTypes: ["Transactions"],
 
   endpoints: (builder) => ({
